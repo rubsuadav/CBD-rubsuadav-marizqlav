934351154fa8778407689d0e282a592110906ca1
rubsuadav <rubsuadav@alum.us.es>
2025-03-10 14:32:18 +0100

    Update Node.js installation script for Linux and add example environment file

--- installNodeLinux.sh
+++ installNodeLinux.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 sudo apt install curl
-curl -o- https://fnm.vercel.app/install | bash
-fnm install 22
-node -v # Should print "v22.14.0".
-npm -v # Should print "10.9.2".
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+sudo apt-get install nodejs
+node -v
+npm -v
