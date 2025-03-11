5d80978a13ba1fa7dfb7cda1c17c51640c91a3ba
rubsuadav <rubsuadav@alum.us.es>
2025-03-11 19:53:09 +0100

    Remove unused components and images from the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // local imports
 import CustomNavbar from "./components/CustomNavbar";
 import { Container } from "react-bootstrap";
-import Home from "./pages/Home";
 import Tasks from "./pages/Tasks";
 import Projects from "./pages/Projects";
 import TaskDetails from "./pages/TaskDetails";
@@ -17,15 +16,14 @@ export default function App() {
       <CustomNavbar />
       <Container>
         <Routes>
-          <Route index element={<Home />}></Route>
-          <Route path="/tasks" element={<Tasks />}></Route>
+          <Route index path="/tasks" element={<Tasks />}></Route>
           <Route path="/task/:taskId" element={<TaskDetails />}></Route>
           <Route path="/projects" element={<Projects />}></Route>
           <Route
             path="/project/:projectId"
             element={<ProjectDetails />}
           ></Route>
-          <Route path="/register" element={<Register />}></Route>
+          <Route index path="/register" element={<Register />}></Route>
           <Route path="/login" element={<Login />}></Route>
         </Routes>
       </Container>
